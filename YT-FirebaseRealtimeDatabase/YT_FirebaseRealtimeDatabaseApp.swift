f4d397a9fe034e1526db74a9c728df384b2fe7c2
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-10-18 05:01:44 +0200

    refactor app file to be more readable

--- YT-FirebaseRealtimeDatabase/YT_FirebaseRealtimeDatabaseApp.swift
+++ YT-FirebaseRealtimeDatabase/YT_FirebaseRealtimeDatabaseApp.swift
@@ -5,28 +5,47 @@
 //  Created by Zaid Neurothrone on 2022-10-18.
 //
 
-import Firebase
-//import FirebaseCore
-import SwiftUI
+//MARK: - UIKit App Delegate Life Cycle Approach
 
-//class AppDelegate: NSObject, UIApplicationDelegate {
-//  func application(
-//    _ application: UIApplication,
-//    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
-//  ) -> Bool {
-//    FirebaseApp.configure()
-//    return true
-//  }
-//}
+/*
+ 
+ import FirebaseCore
+ import SwiftUI
+
+ class AppDelegate: NSObject, UIApplicationDelegate {
+   func application(
+     _ application: UIApplication,
+     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
+   ) -> Bool {
+     FirebaseApp.configure()
+     return true
+   }
+ }
+
+ @main
+ struct YT_FirebaseRealtimeDatabaseApp: App {
+   @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+   
+   var body: some Scene {
+     WindowGroup {
+       ContentView()
+     }
+   }
+ }
+ 
+ */
+
+//MARK: - SwiftUI App Life Cycle Approach
+
+import Firebase
+import SwiftUI
 
 @main
 struct YT_FirebaseRealtimeDatabaseApp: App {
-//  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
-  
   init() {
     FirebaseApp.configure()
   }
-  
+
   var body: some Scene {
     WindowGroup {
       ContentView()
