81896353bfc69997c0e81656eee6848c03dd7289
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-10-18 04:17:32 +0200

    Initial Commit

---
+++ YT-FirebaseRealtimeDatabase/YT_FirebaseRealtimeDatabaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  YT_FirebaseRealtimeDatabaseApp.swift
+//  YT-FirebaseRealtimeDatabase
+//
+//  Created by Zaid Neurothrone on 2022-10-18.
+//
+
+import SwiftUI
+
+@main
+struct YT_FirebaseRealtimeDatabaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
